5480b421903eb2ebce8df2a711ebde5eaabaeddf
Yuxiao Hua <117464811+HuaYuXiao@users.noreply.github.com>
2023-04-14 14:24:00 +0800

    Update graph.m

--- task1/code/graph.m
+++ task1/code/graph.m
@@ -11,6 +11,7 @@ y1_2 = tonevocoder(sig,fs,50,2);
 y1_3 = tonevocoder(sig,fs,50,4);
 y1_4 = tonevocoder(sig,fs,50,6);
 y1_5 = tonevocoder(sig,fs,50,8);
+
 %% To get psd
 [Pxx0,w0] = pwelch(sig,[],[],1024,fs);
 [Pxx1,w1] = pwelch(y1_1,[],[],1024,fs);
@@ -18,6 +19,7 @@ y1_5 = tonevocoder(sig,fs,50,8);
 [Pxx4,w4] = pwelch(y1_3,[],[],1024,fs);
 [Pxx6,w6] = pwelch(y1_4,[],[],1024,fs);
 [Pxx8,w8] = pwelch(y1_5,[],[],1024,fs);
+
 %% plot
 figure(1);
 subplot(3,2,1);plot(f,fftshift(abs(fft(sig))));xlabel('frequency/Hz');ylabel('sig Amp');title('original')
@@ -26,6 +28,7 @@ subplot(3,2,3);plot(f,fftshift(abs(fft(y1_2))));xlabel('frequency/Hz');ylabel('s
 subplot(3,2,4);plot(f,fftshift(abs(fft(y1_3))));xlabel('frequency/Hz');ylabel('sig Amp');title('N=4')
 subplot(3,2,5);plot(f,fftshift(abs(fft(y1_4))));xlabel('frequency/Hz');ylabel('sig Amp');title('N=6')
 subplot(3,2,6);plot(f,fftshift(abs(fft(y1_5))));xlabel('frequency/Hz');ylabel('sig Amp');title('N=8')
+
 figure(2);
 subplot(3,2,1);plot(w0,20*log10(Pxx0));xlabel('frequency/Hz');ylabel('psd/dB');title('original');
 subplot(3,2,2);plot(w1,20*log10(Pxx1));xlabel('frequency/Hz');ylabel('psd/dB');title('N=1');
@@ -33,6 +36,7 @@ subplot(3,2,3);plot(w2,20*log10(Pxx2));xlabel('frequency/Hz');ylabel('psd/dB');t
 subplot(3,2,4);plot(w4,20*log10(Pxx4));xlabel('frequency/Hz');ylabel('psd/dB');title('N=4');
 subplot(3,2,5);plot(w6,20*log10(Pxx6));xlabel('frequency/Hz');ylabel('psd/dB');title('N=6');
 subplot(3,2,6);plot(w8,20*log10(Pxx8));xlabel('frequency/Hz');ylabel('psd/dB');title('N=8');
+
 figure(3)
 subplot(3,2,1);plot(sig);xlabel('time/s');ylabel('sig Amp');title('original');
 subplot(3,2,2);plot(y1_1);xlabel('time/s');ylabel('sig Amp');title('N=1');
